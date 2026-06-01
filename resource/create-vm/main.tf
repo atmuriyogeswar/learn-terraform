815fd5c11de976c3c810893127f0e4553a321fd8
atmuriyogeswar <atmuriyogeswar@gmail.com>
2026-06-01 19:28:18 +0530

    upaded code

--- resource/create-vm/main.tf
+++ resource/create-vm/main.tf
@@ -16,38 +16,34 @@ resource "azurerm_network_interface" "main" {
 }
 
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_linux_virtual_machine" "main" {
   name                  = "test-vm"
   location              = "Denmark East"
   resource_group_name   = "Denmark_East"
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_D2s_v3"
-
-  
- delete_os_disk_on_termination = true
+  size               = "Standard_D2s_v3"
 
  
 
-  storage_image_reference {
-    publisher = "RedHat"
-    offer     = "RHEL "
-    sku       = "101-gen2"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "test-vm"
+  source_image_id ="/subscriptions/3c3ac820-a526-4fd5-841f-cbb2d7ffa483/resourceGroups/Denmark_East/providers/Microsoft.Compute/galleries/image_vm/images/image"
+
+
+  os_disk {
     caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+   storage_account_type = "Standard_LRS"
   }
-  os_profile {
-    computer_name  = "test-vm"
+
+
+ 
     admin_username = "devops"
     admin_password = "Devopsuser1234"
-  }
-  os_profile_linux_config {
+
+
     disable_password_authentication = false
-  }
+
+  secure_boot_enabled = true
+  vtpm_enabled        = true
+
   
 }
 
