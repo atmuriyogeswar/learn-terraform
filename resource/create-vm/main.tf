7ecd9b318e4922e3c97ade2ed929c72fd76c18a4
atmuriyogeswar <atmuriyogeswar@gmail.com>
2026-06-02 02:50:41 +0530

    s

--- resource/create-vm/main.tf
+++ resource/create-vm/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_public_ip" "main" {
   name                = "test-public-ip"
   location            = "Denmark East"
   resource_group_name = "Denmark_East"
-  allocation_method   = "static"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
